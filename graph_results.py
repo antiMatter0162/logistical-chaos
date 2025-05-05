f0a476744982c79350cc030fec25e9285662fe32
antimatter0162 <antimatter5175@gmail.com>
2025-05-05 14:34:02 -0700

    Enhance graphing functionality by setting a window title for trial results and updating the parameters guide with new scenarios and corrected carrying capacity values.

--- graph_results.py
+++ graph_results.py
@@ -7,6 +7,7 @@ def graph_data(params):
         population_data, generation_number = generations.evaluate_population(params, params['Initial Population'])
 
         # Plot the results
+        plt.figure(figsize=(10, 6))
         plt.plot(generation_number, population_data)
         plt.xlabel('Generation Number')
         plt.ylabel('Population Size')
